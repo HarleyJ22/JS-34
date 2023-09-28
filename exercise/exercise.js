6798d3376a7600e1e347a0bad98911e7b4ab7ac6
Miriana Falcone <miriana.falcone7@gmail.com>
2023-09-28 13:20:14 +0200

    modifica

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,7 +1,15 @@
-const printAsyncName = (callBack, n) => {
-    let id = setInterval(callBack, 1000);
-    setTimeout(() => clearInterval(id), 1000 * n);
-}
+function printAsyncName(callback, name) {
+    setTimeout(function() {
+        console.log("Hello");
 
-    printAsyncName(() => console.log("Hello"), 1);
-    setTimeout(() => console.log("Miriana"), 2000);
+    setTimeout(function() {
+        console.log(name);
+    }, 2000);
+
+    callback()
+    }, 1000);
+}
+function myCallback() {
+    console.log("Callback executed");
+}
+printAsyncName(myCallback, "Miriana");
